fe7225f7bde70c6bd110d57450511cf85e5aa82d
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2024-03-13 12:35:03 +0000

    made changes(testing)

--- server/server.js
+++ server/server.js
@@ -5,5 +5,5 @@ const port = 8080;
 app.use(express.static('client'));
 
 app.listen(port, () => {
-    console.log('server has been set up at localhost:', port);
-})
+  console.log('server has been set up at localhost:', port);
+});
